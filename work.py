ee9614a28da644fe0478dc49b71fecf21ca75c3b
rosegithub-dotcom <143061942+rosegithub-dotcom@users.noreply.github.com>
2023-09-22 18:46:32 +0300

    Add files via upload

---
+++ work.py
@@ -0,0 +1,30 @@
+#change 80 to 89
+student_marks=[60,80,90,98]
+student_marks[1]=(89)
+print(student_marks)
+
+#adding anew item to a list
+student_marks=[60,80,90,98] 
+student_marks.append(55)
+print(student_marks)
+
+#finding size of alist
+print(len(student_marks))
+
+#python program to sum up all items in the list
+#summatin of items
+total=sum(student_marks)
+print(total)
+
+#checking liste if true or not
+list_one=input("Enter items of list_one:")
+list_two=input("Enter items of list_two:")
+def common_members(list_one,list_two):
+    result- True
+    for r in list_one:
+        for p in list_two:
+            result= True
+            return result
+print("true there is a common member in list_one and list_two")         
+
+
